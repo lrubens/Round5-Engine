220c5c7e062fa3cd21e2026060f7e690ea509414
rubensl <rubenslacouture@gmail.com>
2019-06-10 14:01:19 -0400

    adding dilithium directory

---
+++ CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/test/engine_check.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/a_fixed.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/a_random.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/cca_encrypt.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/cpa_kem.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/drbg.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/little_endian.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/misc.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/nist_rng.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/pack.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/parameters.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_cca_kem.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_cca_pke.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_core.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_cpa_kem.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_cpa_pke.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_dem.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_hash.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_memory.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/r5_parameter_sets.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/shake.c.o"
+  "CMakeFiles/engine.dir/Round5/reference/src/xef_ref.c.o"
+  "engine.pdb"
+  "engine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
